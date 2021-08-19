461a9e83ad67dd72b8955000db6aa9931943a1c5
Yashika029 <yashikatanwara83@gmail.com>
2021-08-19 18:05:09 +0530

    initial setup

---
+++ requirement.js
@@ -0,0 +1,82 @@
+//  ---- Requirements ----
+
+// Books
+
+//     - ISBN               - String        (International Standard Book No)
+//     - Title              - String
+//     - Author ID          - [Number]
+//     - Language           - String
+//     - Publications       - Number
+//     - No of Pages        - Number
+//     - Categories         - [String]
+
+
+// Author
+
+//      -id       - number
+//      name      - string
+//      books     - [string]
+
+// Publications
+
+//      -id       - number
+//      name      - string
+//      books     - [string]
+
+
+//  ---- APIs ----
+
+// Books 
+
+    //  - GET
+         // - to get all books
+         // - to get specific books
+         // - to get a list of books based on category
+         // - to get a list of books based on author
+
+    //  - POST
+         // to add new  book
+
+    //  - PUT
+         // - to update book details
+         // - to update/add new author
+
+    //  - DELETE
+         // - delete a book
+         // - delete an author from the book 
+         
+         
+// Authors 
+
+    // - GET
+         //  - to get all authors
+         //  - to get specific author
+         //  - to get list of authors based on a book
+
+    // - POST
+         //  - to add new author
+
+    // - PUT
+         //  - update author details
+
+    // - DELETE
+         //  - delete an author
+
+
+// Publications  
+     // - GET
+         //  - to get all publications
+         //  - to get specific publication
+         //  - to get a list of publications based on a book
+
+     // - POST
+         //  - add new publications
+
+     // - PUT
+         //  - update publication details
+         //  - to update/add new book
+
+     // - DELETE 
+         //  - delete a book from publication
+         //  - delete a publication
+
