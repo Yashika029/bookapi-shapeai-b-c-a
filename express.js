be8f43e8f4aea38b8402c582cbf25a4243e55f1d
Yashika029 <yashikatanwara83@gmail.com>
2021-08-25 14:12:29 +0530

    first commit

--- express.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");      // importing express
-
-const ourAPP = express();                 // initializing it
-
-ourAPP.get("/", (request, response) => {
-    response.json({ message: "Request Served!!!!"});
-});
-
-ourAPP.listen(4000, () => console.log("Server is running"));   // listen to the port
